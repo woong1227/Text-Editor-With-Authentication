d401f6b43c5a5a77b0b2a2cec8076937009d17fd
Andy Park <woong1227@gmail.com>
2024-03-15 18:04:12 -0700

    Initial Commit

---
+++ Project28/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project28
+//
+//  Created by Andy Park on 3/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
